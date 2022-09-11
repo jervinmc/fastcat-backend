2903146161edf0a1fe62fe4be2cea617fdb1ada4
Jervin Macalawa <jervinmac>
2022-09-12 00:12:30 +0800

    done

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,7 +5,7 @@ from django.urls.conf import include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from users.views import Login,GetUserView,Signup
-from booking.views import UserBooking
+from booking.views import UserBooking,BuyPaymaya
 from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -20,6 +20,7 @@ path('api/v1/admin/', admin.site.urls),
     path('api/v1/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/auth/user/', GetUserView.as_view(), name='auth_data'),
+    path('api/v1/buy-paymaya/', BuyPaymaya.as_view(), name='auth_data'),
     path('api/v1/login/', Login.as_view(), name='token_refresh'),
     path('api/v1/users/', include('users.urls')),
     path('api/v1/departure/', include('departure.urls')),
--- app/booking/views.py
+++ app/booking/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from users.models import User
 from users.serializers import GetUserSerializer
+import requests
 import pusher
 from decouple import config
 class BookingView(viewsets.ModelViewSet):
@@ -27,3 +28,43 @@ class UserBooking(generics.GenericAPIView):
         model = Booking.objects.filter(user_id = self.request.user.id)
         serializer = BookingSerializer(model,many=True)
         return Response(data = serializer.data)
+
+class BuyPaymaya(generics.GenericAPIView):
+    permission_classes=[permissions.AllowAny]
+    def post(self,request,format=None):
+        res = request.data
+        url = "https://pg-sandbox.paymaya.com/checkout/v1/checkouts"
+        payload = {
+            "totalAmount": {
+                "value": res.get('price'),
+                "currency": "PHP"
+            },
+            "buyer": {
+                "billingAddress": {"countryCode": "GB"},
+                "shippingAddress": {"countryCode": "GB"},
+                "firstName": "Juan",
+                "middleName": "D",
+                "lastName": "Delacruz",
+                "birthday": "2019-10-19"
+            },
+            "items": [
+                {
+                    "amount": {"value": res.get('price')},
+                    "totalAmount": {"value": res.get('price')},
+                    "name": res.get('product'),
+                    "quantity": "1",
+                    "code": "uus",
+                    "description": "dse"
+                }
+            ],
+            "requestReferenceNumber": "123123"
+        }
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Basic cGstWjBPU3pMdkljT0kyVUl2RGhkVEdWVmZSU1NlaUdTdG5jZXF3VUU3bjBBaDo="
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text)
+        return Response(data=response.text)
